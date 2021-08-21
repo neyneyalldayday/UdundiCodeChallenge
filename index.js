fdf383b5cd556fecc25363ac230b7d7275582340
neyneyalldayday <drfrankenstein123@gmail.com>
2021-08-21 17:53:26 -0500

    a little html

---
+++ index.js
@@ -0,0 +1,24 @@
+const toggleClose = () => {
+    let close = document.getElementById("starting-view");
+    if (close.style.display === "none") {
+      close.style.display = "block";
+    } else {
+      close.style.display = "none";
+    }
+  };
+  
+  
+  const plusHandler = () => {
+    toggleClose();
+    toggleDetails();
+  };
+  
+  
+  const toggleDetails = () => {
+    let moreDetails = document.getElementById("more-details");
+    if (moreDetails.style.display === "block") {
+      moreDetails.style.display = "none";
+    } else {
+      moreDetails.style.display = "block";
+    }
+  };
