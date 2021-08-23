954a8d1df4a232726acac32bc230e0b5ff36fcc8
neyneyalldayday <drfrankenstein123@gmail.com>
2021-08-23 16:01:21 -0500

    Updates

---
+++ index.js
@@ -0,0 +1,48 @@
+const toggleClose = () => {
+    let close = document.getElementById("starting-look");
+    if (close.style.display === "none") {
+      close.style.display = "block";
+    } else {
+      close.style.display = "none";
+    }
+  };  
+  
+  const toggleDetails = () => {
+    let moreDetails = document.getElementById("more-details");
+    if (moreDetails.style.display === "block") {
+      moreDetails.style.display = "none";
+    } else {
+      moreDetails.style.display = "block";
+    }
+  };
+
+  const readMore = () => {     
+    let newMessage = document.getElementById("anotherMessage");
+    let moreDetails = document.getElementById("more-details");
+    let close = document.getElementById("starting-look");
+
+    if (newMessage.style.display === "block") {
+      newMessage.style.display = "none";
+    } else {
+      newMessage.style.display = "block";
+    }
+
+    if (newMessage.style.display === "block") {
+      moreDetails.style.display = "none";      
+    } else {
+      close.style.display = "block";      
+    }     
+  };    
+           
+ 
+
+  const plusHandler = () => {
+    toggleClose();
+    toggleDetails();
+   
+  };
+
+  const newMessageHandler = () => {
+    readMore();
+    
+  }
